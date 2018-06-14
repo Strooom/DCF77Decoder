dea1084f3c1623ddabc37b1395d5d1e6515c3875
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2018-06-14 21:13:50 +0200

    reworked towards simpler OO

--- src/DCF77Decoder.cpp
+++ src/DCF77Decoder.cpp
@@ -1,164 +1,239 @@
-DCF77Decoder::DCF77Decoder()
-	{
-	pinMode(DCFsignalInputPin, INPUT);
-	attachInterrupt(DCFinterrupt, intHandler, CHANGE);
-	}
-	
+#include "DCF77Decoder.h"
+#include <Time.h>
+#include <TimeLib.h>
+
+const int DCF77Decoder::BCD[8] = { 1, 2, 4, 8, 10, 20, 40, 80 };				// initialization of a static member of the class
+
+// Public:
+
+DCF77Decoder::DCF77Decoder(uint8_t thePinNmbr) : pinNmbr(thePinNmbr)
+    {
+    pinMode(pinNmbr, INPUT);													// could be INPUT_PULLUP if your antenna has an open-collector output..
+    }
 
 DCF77Decoder::~DCF77Decoder()
-	{
-	// Detach interrupt
-	}
-	
+    {
+    }
 
-void DCF77Decoder::intHandler()
-  {
-  long width;
-  if (!digitalRead(DCFsignalInputPin))
-    {																		 	// signal went 'back to 0' so we just had the end of a pulse
-	width = millis() - msUp;													// measure pulse width as (now() - rising flank)
-    // adjustSignalQuality((width < DCFMaxPulseWidth) 
-    // && (width > DCFMinPulseWidth));											// could additionally check if the width of the pulse is valid
+DCF77SyncState DCF77Decoder::getSyncState()
+    {
+    return syncState;
+    }
 
- 	if ((signalQuality > signalQualityHigh) && msZero)
- 		{
-		iSecond = ((millis()-msZero) / 1000) % 60;								// seconds is calculated as now - beginning of minute
- 		DCFbit[iSecond] = (width > DCFMidPulseWidth);							// write signal bit into buffer
- 		}
-	ready=true;																	// set flag to proceed in loop()
-	}
-  else
-	{  																		 	// signal went up to '1' so we just had the end of a pause
-	width = millis()-msUp;														// measure time between pulses as now() - previous rising flank
-	msUp = millis();															// remember now() for future calculation
-    adjustSignalQuality((width < DCFMaxWidth) && (width > DCFMinWidth));		// check width between pulses to assess signal quality  -should be around 1000 ms
- 	if ((signalQuality > signalQualityHigh) && (width > DCFMinPause59Width))
- 		{																		// detecting decent signalquality and extralong pause -> must be start of a 1-minute cycle
-		msZero = msUp;															// remember 'absolute' time to later calculate offset in seconds of subsequent pulses
-		syncLevel = getSyncLevel();												// perform all built-in checks on decoded signal to assess syncQuality
-		if (syncLevel >= 9)														// if syncQuality is good enough, store decoded time
-			{
-			hours = calcFromBCD(29, 34);
-			minutes = calcFromBCD(21, 27);
-			day = calcFromBCD(36, 41);
-			month = calcFromBCD(45, 49);
-			year = calcFromBCD(50, 57);
-			dayOfWeek = calcFromBCD(42, 44);
-			}
- 		}
-	}
-	}
-  
-  
-  void DCF77Decoder::adjustSignalQuality(boolean valid)
+time_t DCF77Decoder::getTime()
+    {
+
+    }
+
+void DCF77Decoder::run()
+    {
+    boolean currentSignal = digitalRead(pinNmbr);	// what is the signal now
+
+    if (currentSignal && !prevSignal)				// is there a rising edge ?
+        {
+        uint32_t thisRisingEdge = millis();
+        uint32_t gapWidth = thisRisingEdge - lastEdge;
+        adjustSignalQuality((gapWidth > minGapWidth) && (gapWidth < maxGapWidth));
+        adjustSyncState();
+
+        lastEdge = thisRisingEdge;
+
+        if ((signalQuality > signalQualityThreshold) && (gapWidth > minGapWidth59))
+            {
+            switch (syncState)
+                {
+                case DCF77SyncState::searchCycleStart:
+                    cycleStart = thisRisingEdge;
+                    syncState = DCF77SyncState::readingMinute1;
+                    Serial.println("cycle start found");
+                    break;
+                case DCF77SyncState::readingMinute1:
+                    cycleStart = thisRisingEdge;
+                    if (checkData())
+                        {
+                        lastMinute = encodeTime();
+                        Serial.println("first cycle ok");
+                        syncState = DCF77SyncState::readingMinute2;
+                        }
+                    else
+                        {
+                        Serial.print("invalid data");
+                        }
+                    break;
+                case DCF77SyncState::readingMinute2:
+                    cycleStart = thisRisingEdge;
+                    if (checkData())
+                        {
+                         time_t now = encodeTime();
+                        if ((now - lastMinute) == 60)
+                            {
+                            syncState = DCF77SyncState::synced;
+							Serial.println("sync!");
+                            }
+                        else
+                            {
+                            syncState = DCF77SyncState::readingMinute1;
+							Serial.println("cycles mismatch");
+						}
+                        Serial.println(lastMinute);
+                        }
+                    else
+                        {
+                        Serial.print("invalid data");
+                        syncState = DCF77SyncState::readingMinute1;
+                        }
+                    break;
+                case DCF77SyncState::synced:
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    else if (!currentSignal && prevSignal)				// is there a falling egde ?
+        {
+        uint32_t thisFallingEdge = millis();
+        uint32_t pulseWidth = thisFallingEdge - lastEdge;
+
+        adjustSignalQuality((pulseWidth > minPulseWidth) && (pulseWidth < maxPulseWidth));
+        adjustSyncState();
+
+        lastEdge = thisFallingEdge;
+        }
+    prevSignal = currentSignal;
+
+    if ((DCF77SyncState::readingMinute1 == syncState) || (DCF77SyncState::readingMinute2 == syncState))
+        {
+        uint32_t now = millis();
+        uint32_t ms = (now - cycleStart) % 1000;						// How much time has passed since the beginning of the second
+
+        if (ms < 50)
+            {
+            s2 = false;
+            }
+
+        if ((ms > 137) && !s2)
+            {
+            data[((now - cycleStart) / 1000) % 60] = currentSignal;		// Store the sample in the data array
+            sampleTime = millis();
+            s2 = true;
+            }
+        }
+    }
+
+// Private:
+
+void DCF77Decoder::adjustSignalQuality(boolean valid)
     {
     if (valid)
-    	{																		// if pulses/pauses have valid widths (between min AND max), then increase signalQuality counter
-    	signalQuality++;
-    	}
-    else																		// if pulses/pauses have INvalid widths (outside min OR max), then decrease signalQuality counter
-		{
-    	signalQuality--;
-    	signalQuality--;														// signalQuality decrease faster (-2 for each invalid puls/pause), so random pulses definitely decrease it
-		}
-    if (signalQuality > signalQualityMAX)
-    	{
-    	signalQuality = signalQualityMAX;										// max out on (eg) 15 - more levels could be used
-    	}
-    if (signalQuality < 0)
-    	{
-    	signalQuality=0;														// don't go under zero
-    	}
-	}
+        {
+        if (signalQuality < maxSignalQuality)
+            {
+            ++signalQuality;
+            }
+        }
+    else if (signalQuality > 0)
+        {
+        --signalQuality;
+        }
+    }
 
-  int DCF77Decoder::getSyncLevel()
+void DCF77Decoder::adjustSyncState()
     {
-	syncLevel=0;
-	boolean canCompare=true;
-	
- 	if (signalQuality >= signalQualityHigh)										// if we don't receive decent pulses, then syncLevel is down to nothing...
- 		{
-																				// doing every possible check on the received bits. Every positive result increases syncLevel
-		if (!DCFbit[0])															// should be 0
-			{
-			syncLevel++;
-			}
-		if (DCFbit[20])															// should be 1
-			{
-			syncLevel++;
-			}
-		if (!calcParity(21, 28))												// should be even parity
-			{
-			syncLevel++;
-			}
-		if (!calcParity(29, 35))												// should be even parity
-			{
-			syncLevel++;
-			}
-		if (!calcParity(36, 58))												// should be even parity
-			{
-			syncLevel++;
-			}
-		if (calcFromBCD(29, 34) < 60)											// hours should be < 60
-			{
-			syncLevel++;
-			}
-		if (calcFromBCD(21, 27) < 60)											// minutes should be < 60
-			{
-			syncLevel++;
-			}
-		if (calcFromBCD(36, 41) < 32)											// day of month should be < 32
-			{
-			syncLevel++;
-			}
-		if (calcFromBCD(45, 49) < 13)											// month should be < 13
-			{
-			syncLevel++;
-			}
-		if (hours == calcFromBCD(29, 34))										// does previous and current hours value match ?
-			{
-			syncLevel++;
-			}
-		if (day == calcFromBCD(36, 41))											// does previous and current day value match ?
-			{
-			syncLevel++;
-			}
-		if (dayOfWeek == calcFromBCD(42, 44))									// does previous and current dayOfWeek value match ?
-			{
-			syncLevel++;
-			}
-		if (month == calcFromBCD(45, 49))										// does previous and current month value match ?
-			{
-			syncLevel++;
-			}
-		if (year == calcFromBCD(50, 57))										// does previous and current year value match ?
-			{
-			syncLevel++;
-			}
-		}
-	return syncLevel;
-	}
+    if ((signalQuality > signalQualityThreshold) && (DCF77SyncState::searchSteadyPulses == syncState))
+        {
+        syncState = DCF77SyncState::searchCycleStart;
+        Serial.println("stable pulses found");
+        }
 
-boolean DCF77Decoder::calcParity(int from, int to)
-	{
-	boolean parity=false;
-	int i=0;
-	for(i=from;i<=to;i++)
-		{
-		if (DCFbit[i])
-			parity = !parity;
-		}
-	return parity;
-	}
-	
-int DCF77Decoder::calcFromBCD(int from, int to)
-	{
-	int result=0;
-	int i=0;
-	for(i=from;i<=to;i++)
-		{
-		if (DCFbit[i])
-			result = result + BCD[i-from];
-		}
-	return result;
-	}
+    if ((signalQuality < signalQualityThreshold) && (DCF77SyncState::searchSteadyPulses != syncState))
+        {
+        syncState = DCF77SyncState::searchSteadyPulses;
+        Serial.println("stable pulses lost");
+        }
+    }
+
+boolean DCF77Decoder::checkParity(uint8_t from, uint8_t to)
+    {
+    boolean parity=true;
+    for(uint8_t i=from; i<=to; i++)
+        {
+        if (data[i])
+            {
+            parity = !parity;
+            }
+        }
+    return parity;
+    }
+
+uint8_t DCF77Decoder::calcFromBCD(uint8_t from, uint8_t to)
+    {
+    uint8_t result=0;
+    for(uint8_t i=from; i<=to; i++)
+        {
+        if (data[i])
+            {
+            result = result + BCD[i - from];
+            }
+        }
+    return result;
+    }
+
+boolean DCF77Decoder::checkData()
+    {
+    boolean result = true;
+    // Check fixed value bits
+    if (data[0] || (!data[20]) || data[59])
+        {
+        result = false;
+        }
+
+    // Cannot be both CET and CEST
+    if (data[17] && (data[18]))
+        {
+        result = false;
+        }
+
+    // Check Parities
+    if (!checkParity(21, 28) || !checkParity(29, 35) || !checkParity(36, 58))
+        {
+        result = false;
+        }
+
+    // Check time components versus min / max values
+    if (calcFromBCD(21, 27) > 60)
+        {
+        result = false;
+        }
+    if (calcFromBCD(29, 34) > 23)
+        {
+        result = false;
+        }
+    if ((calcFromBCD(36, 41) > 31) || (calcFromBCD(36, 41) == 0))
+        {
+        result = false;
+        }
+    if ((calcFromBCD(45, 49) > 12) || (calcFromBCD(45, 49) == 0))
+        {
+        result = false;
+        }
+    if (calcFromBCD(50, 57) < 18)
+        {
+        result = false;
+        }
+
+    return result;
+    }
+
+time_t DCF77Decoder::encodeTime()
+    {
+    tmElements_t tm;
+    tm.Second = 0;
+    tm.Minute = calcFromBCD(21, 27);
+    tm.Hour = calcFromBCD(29, 34);
+    tm.Day = calcFromBCD(36, 41);
+    tm.Month = calcFromBCD(45, 49);
+    tm.Year = 2000 - 1970 + calcFromBCD(50, 57);
+    return makeTime(tm);
+    }
+
--- src/DCF77Decoder.h
+++ src/DCF77Decoder.h
@@ -1,77 +1,75 @@
-/* ##########################################################################
-   ### DCF77 Library for Arduino                                          ###
-   ### Author : Pascal Roobrouck                                          ###
-   ### License : Creative Commons BY-NC-SA                                ###
-   ### V0.1 - 13-Jan-2015                                                 ###
-   ###                                                                    ###
-   ###                                                                    ###
-   ### Credits/CC : Inspired by Thijs Elenbaas (2012) and                 ###
-   ### ST6 code/experience from Pascal Roobrouck (1989)                   ###
-   ###                                                                    ###
-   ########################################################################## */
-    
-// Constants - Parameters
+// ##########################################################################
+// ### DCF77 Library for Arduino                                          ###
+// ### Author : Pascal Roobrouck                                          ###
+// ### License : Creative Commons BY-NC-SA                                ###
+// ### V1.0.0  15-May-2018                                                ###
+// ##########################################################################
 
-#define DCFShortPulseWidth 103	// width of a short pulse [ms] - Short pulse should be ~100ms so default = 100
-#define DCFLongPulseWidth 185	// width of a long pulse [ms] - Long pulse should be ~200ms so default = 200
-								// 'measure' these pulses (by adding debug statements) and adjust to your HW readings
-#define DCFMidPulseWidth 144	// set to (DCFShortPulseWidth + DCFLongPulseWidth)/2 Default = 150
-#define DCFMinPause59Width 1800	// any pause longer than 1800 is considered to be the missing 59th second
+#ifndef DCF77Decoder_h
+#define DCF77Decoder_h
 
-#define DCFMinPulseWidth 50		// pulses should be min 100ms, shorter than 50ms indicates bad signal reception..
-#define DCFMinPauseWidth 750	// pause should be min 800ms, shorter than 750ms indicates bad signal reception..
-#define DCFMaxPulseWidth 250	// pulses should be max 200ms, longer than 250ms indicates bad signal reception..
-#define DCFMaxPauseWidth 1950	// pause should be max 1900ms, longer than 1950ms indicates bad signal reception..
-								// all these thresholds take an error margin of 50ms, you could adjust these
-#define DCFMinWidth 950			// pulse should be spaced 1000 ms, (except for bit 59->0), shorter indicated bad signal reception
-#define DCFMaxWidth 1050		// pulse should be spaced 1000 ms, (except for bit 59->0), longer indicated bad signal reception
+#include <Arduino.h>
+#include <Time.h>
+#include <TimeLib.h>
 
-#define signalQualityMAX 15		// maximum level of signalQuality variable
-#define signalQualityLow 3		// threshold for 'insufficient' signal quality
-#define signalQualityHigh 12	// threshold for 'sufficient' signal quality
-
-// HardWare
-
-#define DCFsignalInputPin 2	 	// pin on which the digital signal from DCF receiver is to be read
-#define DCFinterrupt 0		 	// Interrupt number associated with DCFsignalInputPin
-
-
-// Classes - Variables
+enum class DCF77SyncState
+    {
+    searchSteadyPulses,
+    searchCycleStart,
+    readingMinute1,
+    readingMinute2,
+    synced,
+    nmbrSyncStates
+    };
 
 class DCF77Decoder
-	{
-	private:
-		// private data
-		const int BCD[8] = {1, 2, 4, 8, 10, 20, 40, 80};						// array of constants, to make simple BCD calculations
-		int seconds;															// counter used to number the pulses in the 1-minute cycle. Set to 0 after detecting the missing pulse at 59, then incremented on each pulse
-		long msUp;																// last time in ms there was a rising signal 0->1
-																				// consecutive values of msUp can calculate spacing of bits, which should be 1000 ms
-																				// millis() - msUp yields the width of a pulse
-		long msZero;															// millis() value at start of a 1-minute cycle, used to easily calculate the relative time-position of signal pulses
-		boolean dataBuffer[60] = {true};										// buffer holding all bits for 1 minute. Initialized to all '1', so it fails most built-in check validations
+    {
+    public:
+        DCF77Decoder(uint8_t thePinNmbr);					// Constructor
+        ~DCF77Decoder();									// Destructor
+
+    private:
+        uint8_t pinNmbr;									// pin number to which our DCF signal is read
+
+        boolean prevSignal = false;							// what was the prvious signal (high or low), so we can detect flanks (rising or falling)
+        uint32_t lastEdge = 0;								// what time was the last rising or falling edge - so we can measure the pulse width / gap width
+
+        const static uint32_t minPulseWidth = 75;			// minimum width of a pulse considered as valid
+        const static uint32_t maxPulseWidth = 225;			// maximum width of a pulse considered as valid
+        const static uint32_t minGapWidth = 775;			// minimum width of a gap considered as valid
+        const static uint32_t maxGapWidth = 925;			// maximum width of a gap considered as valid
+        const static uint32_t minGapWidth59 = 1775;			// minimum width of a gap considered as the missing pulse in last second
+
+        uint8_t signalQuality = 0;							// if pulses have proper timings, this value goes up.. if not, the value goes down.
+        const static uint8_t maxSignalQuality = 24;			// this is the max value for signalQuality
+        const static uint8_t signalQualityThreshold = 16;	// above this value, we consider the DCF-signal to be good enough to start cycle decoding
+
+        DCF77SyncState syncState = DCF77SyncState::searchSteadyPulses;
+
+        uint32_t cycleStart = 0;							// time in millis() when we detected the start of a 1-minute cycle
+        boolean data[60] = { true };						// array holding all bits for 1 minute.
+
+        static const int BCD[8];							// array of constants, to make simple BCD calculations
+
+        time_t lastMinute;									// I thought this was a funny variable name :-)
+        boolean s1, s2, s3;
+        uint32_t sampleTime;
+
+        // private methods
+        void adjustSignalQuality(boolean valid);			// increase/decrease signalQuality when condition is true/false
+        void adjustSyncState();								// TBC
+
+        boolean checkParity(uint8_t from, uint8_t to);		// Calculate even parity over data[from] to data[to]
+        uint8_t calcFromBCD(uint8_t from, uint8_t to);		// Calculate value encoded in data[from] to data[to]
+        boolean checkData();								// Check is data[] contains valid info, based on fixed bits, parity, etc...
+		time_t DCF77Decoder::encodeTime();
+
+    public:
+        void run();											// main workhorse of the decoder : run every 10ms or faster
+        DCF77SyncState getSyncState();						// answers what is the current syncState
+        time_t getTime();									// answers current decoded time, or 0 in case of no sync
+    };
 
 
-int hours;
-int minutes;
-int day;
-int month;
-int year;
-int dayOfWeek;
 
-		
-		// private methods
-		void adjustSignalQuality(boolean valid);
-		int getSyncLevel();
-		boolean calcParity(int from, int to);
-		int calcFromBCD(int from, int to);
-		void intHandler();
-	public:
-		// public data
-		int signalQuality = 0;													// indicates how 'stable' the digitized antenna signal is. By measuring pulse-width, pause-width and puls-spacing and comparing this to preset boundaries
-		int syncQuality = 0;													// indicates how good the sync is, calculated by doing error-checks on decoded signal
-		// public methods - interface
-		DCF77Decoder();															// declaration of constructor
-	};
-
-
-
+#endif // !DCF77Decoder_h
